989aaff2b350ae6a6a4943f718889254085ab2de
Luis Enrique <luisemezag@gmail.com>
2021-10-31 19:02:57 -0600

    Add Interpolation #5

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  itemCount: number = 4;
+  btnText: string = 'Add an item';
+  goalText: string = 'My First Life Goal';
+  goals: string[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.itemCount = this.goals.length;
+  }
+
+  addItem() {
+    this.goals.push(this.goalText);
+    this.goalText = "";
+    this.itemCount = this.goals.length;
   }
 
 }
